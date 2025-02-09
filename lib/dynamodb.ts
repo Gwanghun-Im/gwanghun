de2ada318de8555a2d0d9e727177bacc118db891
Gwanghun-Im <lgh82229047@gmail.com>
2025-02-10 00:02:55 +0900

    env 변경

--- lib/dynamodb.ts
+++ lib/dynamodb.ts
@@ -1,19 +1,27 @@
 import AWS from "aws-sdk"
 
 // 환경 변수 확인 (디버깅)
-console.log("ACCESS_KEY_ID:", process.env.ACCESS_KEY_ID ? "OK" : "MISSING")
+console.log(
+  "ACCESS_KEY_ID:",
+  process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID ? "OK" : "MISSING"
+)
 console.log(
   "SECRET_ACCESS_KEY:",
-  process.env.SECRET_ACCESS_KEY ? "OK" : "MISSING"
+  process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY ? "OK" : "MISSING"
 )
-console.log("REGION:", process.env.REGION ? process.env.REGION : "MISSING")
+console.log(
+  "REGION:",
+  process.env.NEXT_PUBLIC_AWS_REGION
+    ? process.env.NEXT_PUBLIC_AWS_REGION
+    : "MISSING"
+)
 
 // AWS SDK 설정
 const dynamoDb = new AWS.DynamoDB.DocumentClient({
-  region: process.env.REGION,
+  region: process.env.NEXT_PUBLIC_AWS_REGION,
   credentials: {
-    accessKeyId: process.env.ACCESS_KEY_ID,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    accessKeyId: process.env.NEXT_PUBLIC_AWS_REGION,
+    secretAccessKey: process.env.NEXT_PUBLIC_AWS_REGION,
   },
 })
 
