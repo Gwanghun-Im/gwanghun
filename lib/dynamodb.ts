817bf5be4f4cee0a6c7fc85eb47e52831a30ce15
Gwanghun-Im <lgh82229047@gmail.com>
2025-02-10 09:29:01 +0900

    환경변수 오타 수정

--- lib/dynamodb.ts
+++ lib/dynamodb.ts
@@ -20,8 +20,8 @@ console.log(
 const dynamoDb = new AWS.DynamoDB.DocumentClient({
   region: process.env.NEXT_PUBLIC_AWS_REGION,
   credentials: {
-    accessKeyId: process.env.NEXT_PUBLIC_AWS_REGION,
-    secretAccessKey: process.env.NEXT_PUBLIC_AWS_REGION,
+    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
   },
 })
 
