e7eac1b2cddbdcd57ae656af82e7b1ac78ecddc9
Gwanghun-Im <lgh82229047@gmail.com>
2025-04-06 22:41:34 +0900

    FearGreedIndex 컴포넌트 추가 및 GreedFearIndexTemplate에서 기존 로딩 상태 관리 코드 제거하여 컴포넌트 구조 개선

--- components/templates/GreedFearIndexTemplate.tsx
+++ components/templates/GreedFearIndexTemplate.tsx
@@ -1,57 +1,17 @@
-"use client"
-import { useState, useEffect, Suspense } from "react"
+import { Suspense } from "react"
 import { ErrorBoundary } from "react-error-boundary"
-
+import { FearGreedIndex } from "@/components/organisms/FearGreedIndex"
 export const GreedFearIndexTemplate = () => {
-  const [fearGreedIndex, setFearGreedIndex] = useState(null)
-  const [isLoading, setIsLoading] = useState(true)
-
-  const getFearGreedIndex = async () => {
-    try {
-      const response = await fetch("https://api.alternative.me/fng/")
-      const data = await response.json()
-      await new Promise((resolve) => setTimeout(resolve, 1000))
-      setIsLoading(false)
-      return data
-    } catch (error) {
-      console.error("Error:", error)
-    }
-  }
-
-  useEffect(() => {
-    const fetchFearGreedIndex = async () => {
-      const data = await getFearGreedIndex()
-      console.log(data)
-      setFearGreedIndex(data.data[0].value_classification)
-    }
-    fetchFearGreedIndex()
-  }, [])
-
   return (
     <ErrorBoundary fallback={<div>Error</div>}>
       <Suspense
         fallback={
-          <div className="text-center text-2xl font-bold text-white text-opacity-50 flex items-center justify-center bg-blue-900">
+          <div className="text-center text-2xl font-bold text-white text-opacity-50 flex items-center justify-center bg-gray-900">
             Loading...
           </div>
         }
       >
-        <div
-          className={
-            "text-center text-2xl font-bold text-white text-opacity-50 flex items-center justify-center" +
-            (isLoading
-              ? " bg-gray-900"
-              : fearGreedIndex === "Fear"
-              ? " bg-red-500"
-              : " bg-green-500")
-          }
-        >
-          {isLoading ? (
-            "Loading..."
-          ) : (
-            <>Check today&apos;s greed and fear index: {fearGreedIndex}</>
-          )}
-        </div>
+        <FearGreedIndex />
       </Suspense>
     </ErrorBoundary>
   )
